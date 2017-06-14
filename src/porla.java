d1d9b1386d90e4a3c77b01d087c65c2243a2ab46
jhonatanluka <1411032@utp..edu.pe>
2017-06-14 00:46:16 -0500

    Initial commit

---
+++ src/porla.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Usuario on 14/06/2017.
+ */
+public class porla {
+    String ty;
+}
---
+++ src/primero/holas.java
@@ -0,0 +1,8 @@
+package primero;
+
+/**
+ * Created by Usuario on 14/06/2017.
+ */
+public class holas {
+    int gh;
+}
